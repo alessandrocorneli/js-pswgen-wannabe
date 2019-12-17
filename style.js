8246224ec787f1d585c22e49efa071800e3ac6b3
Alessandro <alessandro.corneli11@mail.com>
2019-12-17 16:38:09 +0100

    pwd gen vers.1

---
+++ style.js
@@ -0,0 +1,8 @@
+var nome = prompt("nome");
+console.log(nome);
+var cognome = prompt ("cognome:");
+console.log(cognome);
+var colore = prompt ("colore preferito:");
+console.log(colore);
+var nuovaPassword = nome + cognome + colore + '19';
+document.writeln(nuovaPassword);
